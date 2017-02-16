9101bcbd8d7abda13bc39cef1c949b500f95f31a
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-16 00:29:47 -0500

    Change a valid_age to avoid confusion

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,13 +2,32 @@ puts "What is your name?"
 name_input = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-year = gets.chomp
+year = gets.chomp.to_i
 
 puts "Our cafeteria serves garlic bread. Should we order some for you?"
 garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+
+# make sure age is valid
+
+
+if (2017 - year) == age
+  valid_age = true
+else
+  valid_age = false  
+end  
+
+if valid_age == true && (garlic == "yes" || insurance == "yes")
+  result = "Probably not a vampire."
+elsif valid_age != true && (garlic == "no" || insurance =="no") 
+  result = "Probably a vampire"
+elsif valid_age == false && (garlic == "no" && insurance =="no")
+  result = "Almost certainly a vampire"           
+end  
+
+p result
