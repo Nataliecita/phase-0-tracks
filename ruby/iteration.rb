49702084c6c109c5895a7b795e6302533c66c4fb
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-20 13:42:54 -0500

    Add .each example to hash in iteration.rb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,8 +1,23 @@
-def names
-  name2 = "harini"
-  puts "before code block"
-  yield("natalie",name2)
-  puts "after code block"
+# def names
+#   name2 = "harini"
+#   puts "before code block"
+#   yield("natalie",name2)
+#   puts "after code block"
+# end
+
+# names {|x,name2| puts "#{x} and #{name2} WWOOOOO"}
+
+
+fruits = {"apricot" => "VitaminA",
+          "apple" => "Vitamin B1",
+          "banana" => "Vitamin B2",
+          "blackberries" => "Vitamin B6",
+          "cherries" => "Vitamin C"
+}
+
+# fruits.each { |fruit, vitamin| puts "In this #{fruit} we have #{vitamin}"}
+
+fruits.each do |fruit, vitamin|
+  puts "This is using Do. In this #{fruit} we have #{vitamin}"
 end
 
-names {|x,name2| puts "#{x} and #{name2} WWOOOOO"}
