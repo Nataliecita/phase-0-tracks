1290978cff645128a5aa70f525f17b09435f774c
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-20 10:11:44 -0500

    find methods in method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,16 +4,19 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+
+
+"zom".insert(-2,'o')
 # => “zoom”
 
-# "enhance".<???>
+
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
 # "the usual".<???>
