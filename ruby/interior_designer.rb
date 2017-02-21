2f778245e972d83e0d2dddba0b6b28448c4e13bc
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-21 17:45:27 -0500

    Add interface to interior design to test theories out

---
+++ ruby/interior_designer.rb
@@ -0,0 +1,27 @@
+hash = {
+  name: "Natalie Ruiz",
+  address: "3601 sw 25th",
+  email: "natnatruiz@gmail.com",
+  phone: 3057905296,
+  fav_blue: "indigo",
+  wallpaper_pref: ["paisley", "Abstract woodsy scenes"],
+  ombre: "Fierce"
+}
+
+# p hash
+
+
+# puts hash
+
+hash[:phone] = 305305
+
+# puts hash
+
+hello = hash
+
+ p hello
+
+puts "after"
+ hash[:name] + hash[:address]
+
+ puts hash
