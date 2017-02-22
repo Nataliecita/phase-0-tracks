94e14c9b5ccbabb2a31003f3cbc15969356647d3
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-22 15:49:33 -0500

    Create bball nested data structure in nested_data_structures.rb

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,49 @@
+bball_team = {
+  boys: {
+    jv: {
+      team_info: {
+        guards: 4,
+        forwards: 3,
+        centers: 2 
+      },
+      seniors: [
+        "Bobby",
+        "John",
+        "Weezy"
+      ]
+    },
+    varsity:{
+       team_info: {
+        guards: 4,
+        forwards: 3,
+        centers: 2 
+      },
+        seniors: [
+        "Xavier",
+        "Michael"]
+    } 
+  },
+  girls: {
+    jv:{
+       team_info: {
+        guards: 5,
+        forwards: 4,
+        centers: 1 
+      },
+      seniors:[]
+    },
+    varsity:{
+       team_info: {
+        guards: 6,
+        forwards: 3,
+        centers: 3 
+      },
+      seniors: [
+        "sarah",
+        "Maheen",
+        "Felicia"]
+    }
+  }
+}
+
+puts bball_team
