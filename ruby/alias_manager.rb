5d8f5a6ece32510941e49f82a02429c9ccf67da4
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-23 01:37:51 -0500

    begin pseudocode in alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,24 @@
+# take a spy's real name (e.g) "Felicia Torres"
+# creates a fake name
+
+# swaps the first and last name
+
+# changes all of the vowels to the next vowel in 'aeiou', and all the consonants to the next consonant
+
+# things to consider
+# When will it be helpful to convert the string to an array to work with it more easily?
+# How will you figure out whether a letter is a vowel?
+# How will you deal with the fact that some letters are uppercase?
+# How will you handle edge cases?
+
+# separate into different methods
+# potential methods
+
+# next_vowel
+# next_cons
+
+# get_name_ready
+
+def get_name_ready
+  
+end  
