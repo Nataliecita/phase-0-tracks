f443b0a0224c912420f674ca0d3d71855a43afe2
Natalie Ruiz <natnatruiz@gmail.com>
2017-02-23 02:00:22 -0500

    Write pseudocode for upcoming methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -30,9 +30,15 @@ def get_name_ready(name_input)
   alias_name[0], alias_name[1] = alias_name[1], alias_name[0]
   # change back to string, lowercase, then separate each char
   alias_name = alias_name.join(' ').downcase.chars
-
   # p alias_name
-  
+end  
+
+def to_change(get_name_ready)
+  # see if there needs to be a change
+  # go through each character, if vowel call the vowel method
+
+  # elseif consonant call the consonant vowel,
+  # else do nothing
 end  
 
 
