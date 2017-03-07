c35ffc416c8d4d914f1db8ef18a1bd79e022eea4
Natalie Ruiz <natnatruiz@gmail.com>
2017-03-07 17:12:35 -0500

    Complete release 2 in data_structures.rb

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,21 +1,45 @@
-var colors = ["blue", "red", "green", "yellow"]; 
+var color = ["blue", "red", "green", "yellow"]; 
 
-var names = ["Felicia", "Jason", "Logan", "Tootsie"] ;
+var name = ["Felicia", "Jason", "Logan", "Tootsie"] ;
 
 // console.log(colors);
 // console.log(names);
 
-colors.push("garnet");
+color.push("garnet");
 
-names.push("Edward");
+name.push("Edward");
 
 
 var horses =  {};
 
 
 
-names.forEach(function(value,index){
-  horses[value] = colors[index];
+name.forEach(function(value,index){
+  horses[value] = color[index];
 });
 
-console.log(horses);
+// console.log(horses);
+
+
+var engine = [6, 8];
+
+function Car(name, color, engine) {
+  this.name = name;
+  this.color = color;
+  this.engine = engine;
+
+  this.speed = function() {console.log("VROOOOOOOOM!"); };
+  console.log("Initialization");
+}
+
+
+
+console.log(Car)
+var anotherCar = new Car("Red", "Super red", engine[1]);
+
+console.log(anotherCar)
+anotherCar.speed();
+
+var car2 = new Car(name[3], color[2], engine[0]);
+
+console.log(car2);
