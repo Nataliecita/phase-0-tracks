26a5cd5e7f2f16b834b35f6db6cbfdeda8da1c8a
Natalie Ruiz <natnatruiz@gmail.com>
2017-03-07 15:16:57 -0500

    Complete release 0 in data_structures.js

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var colors = ["blue", "red", "green", "yellow"]; 
+
+var names = ["Felicia", "Jason", "Logan", "Tootsie"] ;
+
+// console.log(colors);
+// console.log(names);
+
+colors.push("garnet");
+
+names.push("Edward");
+
+// console.log(colors, names); 
