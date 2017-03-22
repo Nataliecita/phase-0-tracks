b75a0fecca34b6efce57a15631f16e8df6c20999
Natalie Ruiz <natnatruiz@gmail.com>
2017-03-22 12:30:26 -0400

    Start next release

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -84,3 +84,5 @@ get '/add/:num1/:num2' do
 end
 
 
+# Optional bonus: Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
+
